e06cba45e72335e6bc6f2fb1dbcfd42af4a26cd9
Øystein Schrøder Elvik <oysteine@nbr-oysteine.(none)>
2013-10-15 14:18:37 +0200

    Fixed envvars for pig and hadoop

--- puppet/modules/cassandra/manifests/hadoop.pp
+++ puppet/modules/cassandra/manifests/hadoop.pp
@@ -1,7 +1,7 @@
 class cassandra::hadoop {
   exec {
     'add-cassandra-hadoop-classpath':
-      command => '/bin/echo "export HADOOP_CLASSPATH=/usr/share/cassandra/*:/usr/share/cassandra/lib/*:$HADOOP_CLASSPATH" >> /usr/lib/hadoop/conf/hadoop-env.sh',
+      command => '/bin/echo "export HADOOP_CLASSPATH=/usr/share/cassandra/*:/usr/share/cassandra/lib/*:\$HADOOP_CLASSPATH" >> /usr/lib/hadoop/conf/hadoop-env.sh',
       unless => "/bin/grep 'usr/share/cassandra' /usr/lib/hadoop/conf/hadoop-env.sh",
       require => Package['hadoop'],
   }
--- puppet/modules/hadoop/manifests/pig.pp
+++ puppet/modules/hadoop/manifests/pig.pp
@@ -5,4 +5,17 @@ class hadoop::pig {
     ensure => "latest",
     require => Apt::Ppa['ppa:hadoop-ubuntu/stable'],
   }
+
+  exec {
+    'add-pig-hadoop-envvars':
+      command => '/bin/echo "
+export HADOOP_CLASSPATH=/usr/lib/pig/*:/usr/lib/pig/lib/*:\$HADOOP_CLASSPATH
+export PIG_INITIAL_ADDRESS=localhost
+export PIG_RPC_PORT=9160
+export PIG_PARTITIONER=org.apache.cassandra.dht.RandomPartitioner
+export PIG_OPTS=-Dudf.import.list=org.apache.cassandra.hadoop.pig:\$PIG_OPTS
+	  " >> /usr/lib/hadoop/conf/hadoop-env.sh',
+      unless => "/bin/grep '/usr/lib/pig/*' /usr/lib/hadoop/conf/hadoop-env.sh",
+      require => Package['hadoop'],
+  }
 }
